d99b73a394a77d6135b37cc6f0669e54e052d1f2
Anna Maywald <aniamaywald@gmail.com>
2026-06-13 20:31:57 +0200

    zakomentowanie testu dla EmployeeReport i outputu w App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -16,20 +16,20 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
 
-        // mock data
-        Employee kowalski = new Employee("Kowalski Jan");
-        Employee nowak = new Employee("Nowak Piotr");
-        Project project = new Project("Projekt1");
-
-        List<Task> tasks = new ArrayList<>();
-        tasks.add(new Task("Task1", LocalDate.of(2026, 1, 1), Duration.ofHours(8), kowalski, project));
-        tasks.add(new Task("Task2", LocalDate.of(2026, 1, 2), Duration.ofHours(6), kowalski, project));
-        tasks.add(new Task("Task3", LocalDate.of(2026, 1, 1), Duration.ofHours(4), nowak, project));
-
-        EmployeeReport report = EmployeeReport.generateReport(tasks);
-        EmployeeReportPrinter.printReport(report);
-
-        ProjectReport projectReport = ProjectReport.generateReport(tasks);
-        ProjectReportPrinter.printReport(projectReport);
+        // mock data dla EmployeeReport
+//        Employee kowalski = new Employee("Kowalski Jan");
+//        Employee nowak = new Employee("Nowak Piotr");
+//        Project project = new Project("Projekt1");
+//
+//        List<Task> tasks = new ArrayList<>();
+//        tasks.add(new Task("Task1", LocalDate.of(2026, 1, 1), Duration.ofHours(8), kowalski, project));
+//        tasks.add(new Task("Task2", LocalDate.of(2026, 1, 2), Duration.ofHours(6), kowalski, project));
+//        tasks.add(new Task("Task3", LocalDate.of(2026, 1, 1), Duration.ofHours(4), nowak, project));
+//
+//        EmployeeReport report = EmployeeReport.generateReport(tasks);
+//        EmployeeReportPrinter.printReport(report);
+//
+//        ProjectReport projectReport = ProjectReport.generateReport(tasks);
+//        ProjectReportPrinter.printReport(projectReport);
     }
 }
