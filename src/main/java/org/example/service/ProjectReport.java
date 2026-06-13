e7dc427d05317132d8b1ed1ea14bcd889557e8c2
Anna Maywald <aniamaywald@gmail.com>
2026-06-13 19:34:36 +0200

    Adding ProjectReport.java and a test and output file for it.

---
+++ src/main/java/org/example/service/ProjectReport.java
@@ -0,0 +1,33 @@
+package org.example.service;
+
+import org.example.domain.Project;
+import org.example.domain.Task;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProjectReport {
+
+    public Map<Project, Duration> projects = new HashMap<>();
+
+    public static ProjectReport generateReport(List<Task> tasks) {
+
+        ProjectReport report = new ProjectReport();
+
+        for (Task task : tasks) {
+            Project project = task.getProject();
+            Duration time = task.getWorkingTime();
+
+            if (!report.projects.containsKey(project)) {
+                report.projects.put(project, time);
+            } else {
+                Duration existing = report.projects.get(project);
+                report.projects.put(project, existing.plus(time));
+            }
+        }
+
+        return report;
+    }
+}
