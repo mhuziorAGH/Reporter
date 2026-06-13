521329b0523d951d8679f41ee25fad6f0c7ab7de
zurawskipiotr97-glitch <zurawskipiotr97@gmail.com>
2026-06-13 23:59:25 +0200

    mockito tests

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,10 +8,15 @@ public class App {
         cli.run();
 
 //        testing with params
-//        String [] mockArgs = new String[] {"--r", "R3"};
-//        String [] mockArgs = new String[] {"--r", "R2;","--help", "--path", "/awda", "--r", "R3;", "--r", "R2;" };
-//        CLI cli2 = new CLI(mockArgs);
-//        cli2.run();
+        String [] mockArgs = new String[] {"--path", "Resources/reporter-dane/2012/01/Nowak_Piotr.xls", "--r", "R1"};
+        CLI cli2 = new CLI(mockArgs);
+        cli2.run();
+
+
+
+//        String [] mockArgs3 = new String[] {"--r", "R2;","--help", "--path", "/awda", "--r", "R3;", "--r", "R2;" };
+//        CLI cli3 = new CLI(mockArgs);
+//        cli3.run();
 
     }
 }
