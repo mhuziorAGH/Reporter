3f7437ed3fb91c1a649b25f990958661179c1b4c
MajowyLas <aniamaywald@gmail.com>
2026-06-13 15:42:43 +0200

    Added EmployeeReport.java + test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,36 @@
 package org.example;
 
 import org.example.cli.CLI;
+import org.example.data.FileScanner;
+import org.example.domain.Employee;
+import org.example.domain.Project;
+import org.example.domain.Task;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
         CLI cli = new CLI(args);
+
+        FileScanner fileScanner = new FileScanner();
+        List<List<Object>> list = fileScanner.readExcelFile("/var/home/student/IdeaProjects/Reporter/Resources/reporter-dane/2012/01/Kowalski_Jan.xls");
+
+        for (List<Object> o : list) {
+            String employee = (String) o.get(0);
+            Employee test1 = new Employee(employee);
+            LocalDate date = (LocalDate) o.get(1);
+            String task = (String) o.get(2);
+            Duration time = (Duration) o.get(3);
+            Project project = new Project("Project 1");
+            Task test2 = new Task(task, date, time, test1, project);
+
+            List<Task> test = new ArrayList<>();
+            test.add(test2);
+        }
+
+
     }
 }
--- src/main/java/org/example/service/EmployeeReport.java
+++ src/main/java/org/example/service/EmployeeReport.java
@@ -1,10 +1,14 @@
 package org.example.service;
 
+import org.example.data.FileScanner;
 import org.example.domain.Employee;
+import org.example.domain.Project;
 import org.example.domain.Task;
 
 import java.math.BigDecimal;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,9 +17,25 @@ public class EmployeeReport {
 
     Map<Employee, Duration> employees = new HashMap<>();
 
+  //  FileScanner fileScanner = new FileScanner();
+//    List <List<Object>> list = fileScanner.readExcelFile("/var/home/student/IdeaProjects/Reporter/Resources/reporter-dane/2012/01/Kowalski_Jan.xls");
+//
+//        for (List<Object> o : list) {
+//        String employee = (String) o.get(0);
+//        Employee test1 = new Employee(employee);
+//        LocalDate date = (LocalDate) o.get(1);
+//        String task = (String) o.get(2);
+//        Duration time = (Duration) o.get(3);
+//        Project project = new Project("Project 1");
+//        Task test2 = new Task(task, date, time,test1, project);
+//
 
     public static EmployeeReport generateReport(List<Task> arraylist) {
+
+        FileScanner fileScanner = new FileScanner();
+
         EmployeeReport report = new EmployeeReport();
+
 
         for (Task task : arraylist) {
             Employee employee = task.getEmployee();
@@ -28,8 +48,20 @@ public class EmployeeReport {
                 report.employees.put(task.getEmployee(), newDuration);
             }
 
-
         } return report;
     }
+    }
 
-}
+
+
+
+// for (Task task : arraylist) {
+//Employee employee = task.getEmployee();
+//Duration time = task.getWorkingTime();
+//            if (!report.employees.containsKey(employee)) {
+//        report.employees.put(employee, time);
+//            } else {
+//Duration duration = report.employees.get(task.getEmployee());
+//Duration newDuration = duration.plus(time);
+//                report.employees.put(task.getEmployee(), newDuration);
+//        }
