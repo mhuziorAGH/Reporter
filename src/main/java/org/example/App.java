3b531f5dc7eaa4c64d13af43bf70995f2adf8d35
Anna Maywald <aniamaywald@gmail.com>
2026-06-13 21:01:49 +0200

    test w APP, na fetch realincyh danych

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.data.FileScanner;
 import org.example.domain.Employee;
 import org.example.domain.Project;
 import org.example.domain.Task;
@@ -16,20 +17,10 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
 
-        // mock data dla EmployeeReport
-//        Employee kowalski = new Employee("Kowalski Jan");
-//        Employee nowak = new Employee("Nowak Piotr");
-//        Project project = new Project("Projekt1");
-//
-//        List<Task> tasks = new ArrayList<>();
-//        tasks.add(new Task("Task1", LocalDate.of(2026, 1, 1), Duration.ofHours(8), kowalski, project));
-//        tasks.add(new Task("Task2", LocalDate.of(2026, 1, 2), Duration.ofHours(6), kowalski, project));
-//        tasks.add(new Task("Task3", LocalDate.of(2026, 1, 1), Duration.ofHours(4), nowak, project));
-//
-//        EmployeeReport report = EmployeeReport.generateReport(tasks);
-//        EmployeeReportPrinter.printReport(report);
-//
-//        ProjectReport projectReport = ProjectReport.generateReport(tasks);
-//        ProjectReportPrinter.printReport(projectReport);
+        FileScanner fileScanner = new FileScanner();
+        List<Task> tasks = fileScanner.readExcelFile("Resources/reporter-dane/2012/01/Kowalski_Jan.xls");
+
+        EmployeeReport report = EmployeeReport.generateReport(tasks, LocalDate.of(2010, 1, 1), LocalDate.of(2030, 1, 1));
+        EmployeeReportPrinter.printReport(report);
     }
 }
