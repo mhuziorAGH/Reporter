d39c2a79ef0cf38516357f86c185452f76aabadf
MajowyLas <aniamaywald@gmail.com>
2026-06-13 15:55:55 +0200

    Added EmployeeReport.java + test

---
+++ src/main/java/org/example/output/EmployeeReportPrinter.java
@@ -0,0 +1,10 @@
+package org.example.output;
+
+import org.example.service.EmployeeReport;
+
+public class EmployeeReportPrinter {
+
+    public static void printReport(EmployeeReport employeeReport) {
+
+    }
+}
