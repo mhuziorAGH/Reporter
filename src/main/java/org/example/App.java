9a4856b431d3ae8315a882722013b43d4b667d7d
zurawskipiotr97-glitch <zurawskipiotr97@gmail.com>
2026-06-13 14:37:06 +0200

    app

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,15 +2,8 @@ package org.example;
 
 import org.example.cli.CLI;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 public class App {
     public static void main(String[] args) {
         CLI cli = new CLI(args);
-
-        LocalDate jutro = LocalDate.now().plusDays(365);
-        DateTimeFormatter formater = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        System.out.println(jutro.format(formater));
     }
 }
