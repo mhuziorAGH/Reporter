459c6380be57f54d3c4c9941f5f336da91af6085
Anna Maywald <aniamaywald@gmail.com>
2026-06-13 21:38:45 +0200

    Rozszerzenie EmployeeReport.java o projekty oraz kalkulacje % czasu spedzonych w projektach oer employee

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -16,20 +16,20 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
 
-        // mock data dla EmployeeReport
-//        Employee kowalski = new Employee("Kowalski Jan");
-//        Employee nowak = new Employee("Nowak Piotr");
-//        Project project = new Project("Projekt1");
-//
-//        List<Task> tasks = new ArrayList<>();
-//        tasks.add(new Task("Task1", LocalDate.of(2026, 1, 1), Duration.ofHours(8), kowalski, project));
-//        tasks.add(new Task("Task2", LocalDate.of(2026, 1, 2), Duration.ofHours(6), kowalski, project));
-//        tasks.add(new Task("Task3", LocalDate.of(2026, 1, 1), Duration.ofHours(4), nowak, project));
-//
-//        EmployeeReport report = EmployeeReport.generateReport(tasks);
-//        EmployeeReportPrinter.printReport(report);
-//
-//        ProjectReport projectReport = ProjectReport.generateReport(tasks);
-//        ProjectReportPrinter.printReport(projectReport);
+        Employee kowalski = new Employee("Kowalski Jan");
+        Employee nowak = new Employee("Nowak Piotr");
+        Project projekt1 = new Project("Projekt1");
+        Project projekt2 = new Project("Projekt2");
+
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task("Task1", LocalDate.of(2026, 1, 1), Duration.ofHours(8), kowalski, projekt1));
+        tasks.add(new Task("Task2", LocalDate.of(2026, 1, 2), Duration.ofHours(6), kowalski, projekt2));
+        tasks.add(new Task("Task3", LocalDate.of(2026, 1, 1), Duration.ofHours(4), nowak, projekt1));
+
+        EmployeeReport report = EmployeeReport.generateReport(tasks);
+        EmployeeReportPrinter.printReport(report);
+
+        ProjectReport projectReport = ProjectReport.generateReport(tasks);
+        ProjectReportPrinter.printReport(projectReport);
     }
 }
