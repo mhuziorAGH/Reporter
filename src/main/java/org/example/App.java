4c01eb12773570db966a31f2fb578fc4de8b600b
zurawskipiotr97-glitch <zurawskipiotr97@gmail.com>
2026-06-14 12:25:02 +0200

    to do ;

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -18,8 +18,8 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-//        CLI cli = new CLI(args);
-//        cli.run();
+        CLI cli = new CLI(args);
+        cli.run();
 
 //        testing with params
 //        String [] mockArgs = new String[] {"--path", "Resources/reporter-dane/2012/01/Kowalski_Jan.xls", "--r", "R1", "--from", "2012-01-01", "--to", "2012-03-31;",
@@ -29,9 +29,9 @@ public class App {
 //        CLI cli2 = new CLI(mockArgs);
 //        cli2.run();
 
-        String[] mockArgs = new String[] {"--path", "Resources/reporter-dane/2012/02/Kowalski_Jan.xls", "--r", "R4"};
-        CLI cli = new CLI(mockArgs);
-        cli.run();
+//        String[] mockArgs = new String[] {"--path", "Resources/reporter-dane/2012/02/Kowalski_Jan.xls", "--r", "R4"};
+//        CLI cli = new CLI(mockArgs);
+//        cli.run();
 
 
 //        String [] mockArgs3 = new String[] {"--r", "R2;","--help", "--path", "/awda", "--r", "R3;", "--r", "R2;" };
