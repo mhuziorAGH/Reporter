6b86955d238ea03fd5628d51f6ace4e638cd4408
Anna Maywald <aniamaywald@gmail.com>
2026-06-14 13:22:11 +0200

    Podpiecie raportu nr 2 do CLI

--- src/main/java/org/example/cli/ReportProjectsCommand.java
+++ src/main/java/org/example/cli/ReportProjectsCommand.java
@@ -1,7 +1,24 @@
 package org.example.cli;
 
+import org.example.data.ExcelFileFinder;
+import org.example.domain.Task;
+import org.example.output.ProjectReportPrinter;
+import org.example.output.TaskReportPrinter;
+import org.example.service.ProjectReport;
+import org.example.service.TaskReport;
+
+import java.util.List;
+
 public class ReportProjectsCommand {
     public static void execute(ParamsSet params) {
-        System.out.println("ReportProjectsCommand");
+
+        ExcelFileFinder excelFileFinder = new ExcelFileFinder();
+        List<Task> tasks = excelFileFinder.run(params);
+
+        new ProjectReport();
+       ProjectReport projectReport = ProjectReport.generateReport(tasks);
+
+       ProjectReportPrinter.printReportProject(projectReport);
+
     }
 }
